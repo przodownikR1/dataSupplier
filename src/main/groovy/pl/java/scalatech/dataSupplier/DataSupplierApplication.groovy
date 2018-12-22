ae0b27417fe9465cd3fdf7a17cac36c7468a202b
slawek <przodownikr1@gmail.com>
2018-12-22 11:36:56 +0100

    initial project

---
+++ src/main/groovy/pl/java/scalatech/dataSupplier/DataSupplierApplication.groovy
@@ -0,0 +1,14 @@
+package pl.java.scalatech.dataSupplier
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DataSupplierApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DataSupplierApplication, args)
+	}
+
+}
+
---
+++ src/test/groovy/pl/java/scalatech/dataSupplier/DataSupplierApplicationTests.groovy
@@ -0,0 +1,17 @@
+package pl.java.scalatech.dataSupplier
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class DataSupplierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
